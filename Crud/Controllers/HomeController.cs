aaefe4b50ec9b6e9052c3d1f13959484f3332900
francois227 <francoismaelparedes@hotmail.com>
2023-03-07 18:50:58 -0300

    primer commit

---
+++ Crud/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Crud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Persons()
+        {
+            var persons = new List<Person>();
+            persons.Add(new Person { Nombre = "Pepe", Edad = 10 });
+            persons.Add(new Person { Nombre = "Jaimito", Edad = 30 });
+            persons.Add(new Person { Nombre = "Juanito", Edad = 20 });
+            return View(persons);
+        }
+    }
+}
---
+++ Crud/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Crud.Models
+{
+    public class Person
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
